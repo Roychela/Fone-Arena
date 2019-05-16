a3cdd43578e693579a055665df02e9b3fd3214ef
Marion Ambiyo <marionambiyo@Adielas-MacBook-Pro.local>
2019-05-16 15:23:26 +0300

    Update scripts

---
+++ js/scripts2.js
@@ -0,0 +1,16 @@
+// floating action button//
+$(document).ready(function(){
+    $('.fixed-action-btn').floatingActionButton();
+    $('.dropdown-trigger').dropdown();
+  });
+
+  $("#submitbutton").click(function() {
+    var messageHere = (document.getElementById("messageHere").value);
+    var name = (document.getElementById("name").value);
+    var email =(document.getElementById("email").value);
+    if(messageHere != "") { alert(name + " your message is received. Thank you for the review.");
+  } else { alert ("Fill out form and submit");
+  }
+  });
+  
+        
