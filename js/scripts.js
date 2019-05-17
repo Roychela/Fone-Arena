802b88320b4ddffb148546f5bd3d2e3729a630e7
Roy Chela <roychela@gmail.com>
2019-05-17 09:48:36 +0300

    added a comment section

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,30 @@
     $('.fixed-action-btn').floatingActionButton();
 
  });
+ function resetFields() {
+   $("input#name").val("");
+   $("input#email").val("");
+   $("#message").val("");
+
+}
 
 $("#submitbutton").click(function () {
   var messageHere = (document.getElementById("messageHere").value);
   var name = (document.getElementById("name").value);
   var email = (document.getElementById("email").value);
+  var comment = (document.getElementById("message").value);
   if (messageHere != "") {
     alert(name + " your message is received. Thank you for the review.");
   } else {
     alert("Fill out form and submit");
   }
+  $(".reviews").append(
+                           '<div class="new-reviws">' +
+                             '<h4>Name</h4>'+ name +
+                             '<h4>comment</h4>'+ comment
+                           +'</div>'
+
+                           );
+
+ resetFields();
 });
